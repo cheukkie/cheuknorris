a55208b1344076a6a6358c1b4bc2e75b5697ec83
cheukkie <Cheukkie@gmail.com>
2019-04-24 00:08:57 +0200

    Added style loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: {
-        app: ['@babel/polyfill','./src/app.js']
+        app: ['@babel/polyfill','./src/js/app.js']
     },
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['@babel/preset-env']
-            }
-        }]
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                        "style-loader",
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                        "style-loader",
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    ]
+                }
+
+        ]
     }
 }
