cf3e2b445492f9e3ea371e12189ee5ee8464171a
cheukkie <Cheukkie@gmail.com>
2019-04-25 22:00:12 +0200

    Added button to fetch 10 random jokes

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,58 @@
 import 'normalize.css';
 import '../scss/main.scss';
 
-const app = new Vue({
-    el: '#app',
+/*
+
+TO DO:
+
+- GET FETCH JOKES BUTTON
+
+- SAVE FAVORITE LIST TO LOCALSTORAGE
+- CLEAR LOCALSTORAGE BUTTON
+- CONFIRMATION ON REMOVE JOKE
+
+- ONLY UNIQUE JOKES IN FAVORITE
+    > CHECK IF JOKE EXISTS IN FAV LIST
+    > GET 10 UNIQUE JOKES BY CHECKING WITH FAV LIST
+
+- BUTTON TIMER 5s FETCHING JOKES UNTIL FAV LIST REACH MAX
+
+
+CSS STYLING + ANIMATION:
+- LOADING ANIMATION
+
+*/
+
+const chuckNorris = new Vue({
+    el: '#chuckNorris',
     data: {
         jokes: [],
         favorites: []
     },
     methods: {
+        getRandomJokes: async function(qty) {
+            const response = await fetch('http://api.icndb.com/jokes/random/'+qty);
+            const data = await response.json();            
+            return data.value;
+        },
+        showRandomJokes: function(qty){
+            this.getRandomJokes(qty)
+            .then(data => {
+                this.jokes = [];
+                data.forEach(joke => {
+                    if( this.jokes.length <= 9 ){
+                        this.jokes.push({
+                            id : joke.id,
+                            joke : joke.joke,
+                            categories : joke.categories,
+                            faved : false
+                        });
+                    }
+                });
+            });
+        },
         addFav: function(joke) {
-            if( this.favorites.length <= 10 ){
+            if( this.favorites.length <= 9 ){
                 joke.faved = true;
                 this.favorites.push(joke);
             }else{
@@ -30,19 +73,4 @@ const app = new Vue({
 });
 
 
-async function getPosts() {
-    const response = await fetch('http://api.icndb.com/jokes/random/10');
 
-    const data = await response.json();
-    return data.value;
-}
-getPosts().then(allJokes => {
-    allJokes.forEach(joke => {
-        app.jokes.push({
-            id : joke.id,
-            joke : joke.joke,
-            categories : joke.categories,
-            faved : false
-        });
-    });
-});
